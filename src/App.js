774bf46bf99aba1f993fb6008482e1d5d418096b
Immanuel Huang <immanuelgyhuang@gmail.com>
2021-01-09 21:07:31 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Chessground from "react-chessground";
+import "react-chessground/dist/styles/chessground.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Chessground
+        coordinates={false}
+        viewOnly={true}
+        fen={"8/8/8/8/8/8/8/8 w - - 0 1"}
+      />
     </div>
   );
 }
