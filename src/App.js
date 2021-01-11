41f00ae2e7735ee9f2761fddf8f4bfa1b25d4f90
Immanuel Huang <immanuelgyhuang@gmail.com>
2021-01-11 14:48:17 -0500

    Implemented displaying of solutions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 import PermutationBoard from "./components/PermutationBoard.js";
+import { useState } from "react";
+import Chessground from "react-chessground";
+import "react-chessground/dist/styles/chessground.css";
+import uniqid from "uniqid";
 
 const App = () => {
+  const [solutions, setSolutions] = useState([]);
+
   return (
     <div>
-      <PermutationBoard />
+      <PermutationBoard solutions={solutions} setSolutions={setSolutions} />
+      {solutions.map((board) => (
+        <div key={uniqid()} style={{ margin: "10px" }}>
+          <Chessground
+            animation={{ enabled: false }}
+            fen={board}
+            coordinates={false}
+            viewOnly={true}
+          />
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/PermutationBoard.js
+++ src/components/PermutationBoard.js
@@ -5,27 +5,22 @@ import arrToFEN from "./utils/arrToFEN.js";
 import initializeBoard from "./utils/initializeBoard.js";
 import isSolution from "./utils/isSolution.js";
 
-const PermutationBoard = () => {
-  let queens = useRef([0, 0, 0, 0, 0, 0, 2, 0]);
-  const [board, setBoard] = useState([
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-    ["X", "_", "_", "_", "_", "_", "_", "_"],
-  ]);
+const PermutationBoard = (props) => {
+  const { solutions, setSolutions } = props;
+  let queens = useRef([0, 3, 5, 7, 2, 0, 6, 4]);
+  const [board, setBoard] = useState(
+    (() => {
+      const tempBoard = initializeBoard();
+      for (let i = 0; i < 8; i++) tempBoard[i][queens.current[i]] = "X";
+      return tempBoard;
+    })()
+  );
   let tempBoard = useRef();
 
   function updateQueen() {
-    if (queens.current.every((queen) => queen === 0)) {
-      console.log("START");
-    }
-    if (queens.current.every((queen) => queen === 7)) {
-      console.log("DONE");
-    } else {
+    if (queens.current.every((queen) => queen === 0)) console.log("START");
+    if (queens.current.every((queen) => queen === 7)) console.log("DONE");
+    else {
       queens.current[0]++;
       while (queens.current.includes(8)) {
         const index = queens.current.indexOf(8);
@@ -33,16 +28,18 @@ const PermutationBoard = () => {
         queens.current[index + 1]++;
       }
       tempBoard.current = initializeBoard();
-      for (let i = 0; i < 8; i++) {
-        tempBoard.current[i][queens.current[i]] = "X";
-      }
+      for (let i = 0; i < 8; i++) tempBoard.current[i][queens.current[i]] = "X";
       setBoard(JSON.parse(JSON.stringify(tempBoard.current)));
-      if (isSolution(board) === true) console.log(board);
+      if (isSolution(board) === true)
+        setSolutions(solutions.concat(arrToFEN(board)));
     }
   }
 
   useEffect(() => {
-    setTimeout(updateQueen, 0);
+    const timer = setTimeout(() => {
+      updateQueen();
+    }, 50);
+    return () => clearTimeout(timer);
   });
 
   return (
